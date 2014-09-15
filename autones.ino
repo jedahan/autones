cc6c3db100573508f180668251699855edbf5cf0
Jonathan Dahan <jonathan@jedahan.com>
2014-09-15 15:45:24 -0400

    hardcode everything

--- autones.ino
+++ autones.ino
@@ -1,17 +1,11 @@
-byte button[] = { 8, 1};
-byte frames[] = { 1, 0};
-
 #define clockInterrupt 0
 #define latchInterrupt 1
 
 #define dataShift 2
 #define dataMask (1 << dataShift)
 
-byte buttons = 0;
 int frame = 0;
-int index = 0;
-
-int nextframe = frames[0];
+int buttons = ~(8);
 
 void setup() {
   pinMode(A2, OUTPUT);
@@ -23,12 +17,12 @@ void setup() {
 }
 
 void latch() {
-  frame++;
-  if(frame > nextframe && index < sizeof(frames)-1 ){
-    index++;
-    nextframe += frames[index];
+  if(frame < 1){
+    frame++;
   }
-  buttons = ~button[index];
+  if(frame >= 1){
+    buttons = ~(1);
+  }
   PORTC = (PORTC & ~dataMask) | ((buttons & 1) << dataShift);
   buttons >>= 1;
 }
