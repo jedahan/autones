6c3b255009c87ec968d4fb947d6e934da52f8b1f
Jonathan Dahan <jonathan@jonathan.is>
2015-11-18 18:55:16 -0500

    first second working version

--- autones.c
+++ autones.c
@@ -1,13 +1,12 @@
-#include <avr/interrupt.h>
 #include <avr/pgmspace.h>
 #include <avr/io.h>
-
-#define loop true
+#include <avr/interrupt.h>
+#include <util/delay.h>
 
 uint8_t button_number;
 // we are using naive run-length encoding
 // frames[0] corresponds to how many frames we should be pressing buttons[0]
-PROGMEM const uint16_t frames[] = {10,10,10,10};
+PROGMEM const uint16_t frames[] = {30,1,30,1,30,10,30,10,30,10,30,10,30};
 // buttons are A B s S U D L R
 #define A_ 1 << 7
 #define B_ 1 << 6
@@ -18,12 +17,13 @@ PROGMEM const uint16_t frames[] = {10,10,10,10};
 #define L_ 1 << 1
 #define R_ 1 << 0
 
-PROGMEM const uint8_t buttons[] =  {U_,R_,D_,L_};
+PROGMEM const uint8_t buttons[] =  {0,S_,0,S_,0,U_,0,R_,0,D_,0,L_,0};
 
 uint16_t movie_size = sizeof(buttons);
 uint16_t index = 0;
 
-uint16_t frame = frames[0];
+uint8_t slowdown = 60;
+uint16_t frame = slowdown * frames[0];
 uint8_t button = ~(buttons[0]);
 
 int main(void) {
@@ -35,34 +35,49 @@ int main(void) {
   EICRA = (1<<ISC11) | (1<<ISC10);
   // enable interrupt 1 and interrupt 0
   EIMSK = (1<<INT1) | (1<<INT0);
+  // enable timer 0
+  TIMSK0 |= (1 << TOIE0);
+  // set timer 0 mode to CTC
+  TCCR0A |= (1 << WGM01);
+  // count to 0xF9 (249)
+  OCR0A = 0xF9;
+  // set the ISR COMPA vect
+  TIMSK0 |= 1 << OCIE0A;
   // global interrupt enable
   sei();
 
   while(1){
-    if(index == movie_size) {
-#ifdef loop
+    if(index >= movie_size) {
       index = 0;
-#else
-        cli();
-#endif
+      PORTB ^= _BV(PB0); // toggle LED
     }
   };
   return 1;
 }
 
-// latch
+// every time we see a latch, reset the current buttons
 ISR(INT0_vect) {
-  if(frame == 0){
+  // reset to the current button
+  button = ~(pgm_read_byte_near(buttons + index));
+  TCCR0B &= ~(1 << CS02); // disable this timer
+  TCCR0B |= (1 << CS02); // set prescaler to 256, to start the 4ms timer
+  PORTB ^= _BV(PB0); // toggle LED
+}
+
+// input timeout for timer0
+// once we have heard the last latch in a while, advance the frame
+ISR (TIMER0_OVF_vect) {
+  TCCR0B &= ~(1 << CS02); // disable this timer
+  if(frame == 0) { // run-length decoding, to advance the index
     index++;
-    frame = pgm_read_word_near(frames + index);
-    button = ~(pgm_read_byte_near(buttons + index));
+    frame = slowdown * pgm_read_word_near(frames + index);
   }
   PORTB = button;
   button >>= 1;
   frame--;
 }
 
-// clock
+// on clock, shift out each button
 ISR(INT1_vect) {
   _delay_ms(1);
   PORTB = button;
